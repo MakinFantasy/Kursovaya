74942f7f49166703aa559e9fbab81e22432c54af
MakinFantasy <beingp0z1t1v3@gmail.com>
2023-01-30 03:17:58 +0300

    finished

--- kurs/polls/static/polls/js/index.js
+++ kurs/polls/static/polls/js/index.js
@@ -1,17 +1,18 @@
 document.getElementById('submit').addEventListener('click', submitFunc)
 
 function submitFunc(event) {
+    event.preventDefault()
     const year = 2023;
     let first = document.getElementById('year')
     let second = document.getElementById('country')
     let third = document.getElementById('materials')
     let fourth = document.getElementById('unique')
     let field = document.getElementById('main')
-    let secField = document.getElementById('secondary')
+    let secField = document.getElementById('price')
     if (second.value == 'Rus') {
         console.log(year, parseInt(first.value), year - parseInt(first.value))
         if (year - parseInt(first.value) > 100 ) {
-            field.innerText = 'Ваш предмет считается антиквариатом, но его нельзя вывозить из-за странны по закону <О вывозе и ввозе культурных ценностей>'
+            field.innerText = "Ваш предмет считается антиквариатом, но его нельзя вывозить из-за странны по закону 'О вывозе и ввозе культурных ценностей'"
         } else if (year - parseInt(first.value) > 60) {
             field.innerText = 'Ваш предмет считается антиквариатом'
         } else if (year - parseInt(first.value) < 60 && (year - parseInt(first.value) > 15)) {
@@ -24,7 +25,7 @@ function submitFunc(event) {
     if (second.value == 'UK') {
         console.log(year, parseInt(first.value), year - parseInt(first.value))
         if (year - parseInt(first.value) > 100 ) {
-            field.innerText = 'Ваше предмет считается антиквариатом'
+            field.innerText = 'Ваш предмет считается антиквариатом'
         } else if (year - parseInt(first.value) >= 50 && year - parseInt(first.value) <= 100) {
             field.innerText = 'Ваш предмет не считается антиквариатом, но может быть оценен как винтаж'
         } else {
@@ -66,5 +67,31 @@ function submitFunc(event) {
     }
 
     // secondary
-    event.preventDefault()
+
+    if (third.value == '?') {
+        if (fourth.value == 'no') {
+            secField.innerText = 'Цена за ваш предмет зависит от редкости предмета'
+        } else if (fourth.value == 'yes') {
+            secField.innerText = 'Цена за ваш предмет будет не слишком высокой, все зависит от технологии изготовления'
+        } else if (fourth.value == 'mb') {
+            secField.innerText = 'Цену за ваш предмет определить сложно'
+        }
+    } else if (third.value == 'no') {
+        if (fourth.value == 'no') {
+            secField.innerText = 'Цена за ваш предмет сильно зависит от редкости предмета'
+        } else if (fourth.value == 'yes') {
+            secField.innerText = 'Технология изготовления может сильно повлиять на стоимость предмета в лучшую сторону'
+        } else if (fourth.value == 'mb') {
+            secField.innerText = 'Цену за ваш предмет определить сложно'
+        }
+    } else if (third.value == 'yes') {
+        if (fourth.value == 'no') {
+            secField.innerText = 'Цена за ваш предмет может оказать высокой из-за редких или дорогих материалов'
+        } else if (fourth.value == 'yes') {
+            secField.innerText = 'Цена на ваш товар будет высокой'
+        } else if (fourth.value == 'mb') {
+            secField.innerText = 'Материалы могут сильно повлиять на стоимость предмета в лучшую сторону'
+        }
+    }
+
 }
